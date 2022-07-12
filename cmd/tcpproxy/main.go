9be32beff146d88328cbd0c92825b390277ec3cb
devincd <505259926@qq.com>
2022-07-12 19:29:06 +0800

    fix bug
    Signed-off-by: devincd <505259926@qq.com>

--- cmd/tcpproxy/main.go
+++ cmd/tcpproxy/main.go
@@ -6,52 +6,44 @@ import (
 	"io"
 	"log"
 	"net"
-	"sync"
 )
 
-var localAddr *string = flag.String("local", "0.0.0.0:80", "local address")
-var remoteAddr *string = flag.String("remote", "0.0.0.0:80", "remote address")
+var localAddr *string = flag.String("l", "localhost:9999", "local address")
+var remoteAddr *string = flag.String("r", "localhost:80", "remote address")
 
 func main() {
 	flag.Parse()
 	fmt.Printf("Listening: %v\nProxying: %v\n\n", *localAddr, *remoteAddr)
 
-	listener, err := net.Listen("tcp4", *localAddr)
+	listener, err := net.Listen("tcp", *localAddr)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	defer listener.Close()
 	for {
 		conn, err := listener.Accept()
-		log.Println("[New connection] remote addr:", conn.RemoteAddr())
+		log.Println("New connection", conn.RemoteAddr())
 		if err != nil {
 			log.Println("error accepting connection", err)
 			continue
 		}
-		go DoProxy(conn)
+		go func() {
+			defer conn.Close()
+			conn2, err := net.Dial("tcp", *remoteAddr)
+			if err != nil {
+				log.Println("error dialing remote addr", err)
+				return
+			}
+			defer conn2.Close()
+			closer := make(chan struct{}, 2)
+			go copy(closer, conn2, conn)
+			go copy(closer, conn, conn2)
+			<-closer
+			log.Println("Connection complete", conn.RemoteAddr())
+		}()
 	}
 }
 
-func DoProxy(conn net.Conn) {
-	conn2, err := net.Dial("tcp4", *remoteAddr)
-	if err != nil {
-		log.Println("error dialing remote addr", err)
-		return
-	}
-	defer conn2.Close()
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		// send data
-		io.Copy(conn2, conn)
-		wg.Done()
-	}()
-	wg.Add(1)
-	go func() {
-		// receive data
-		io.Copy(conn, conn2)
-		wg.Done()
-	}()
-	wg.Wait()
-	log.Println("[Connection complete] remote addr:", conn.RemoteAddr())
+func copy(closer chan struct{}, dst io.Writer, src io.Reader) {
+	_, _ = io.Copy(dst, src)
+	closer <- struct{}{} // connection is closed, send signal to stop proxy
 }
