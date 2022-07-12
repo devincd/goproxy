b5993ba7b7b55433a90fd1a3bd59fb7b551b1d64
devincd <505259926@qq.com>
2022-07-12 18:41:26 +0800

    add tcp proxy
    Signed-off-by: devincd <505259926@qq.com>

--- cmd/tcpproxy/main.go
+++ cmd/tcpproxy/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"sync"
+)
+
+var localAddr *string = flag.String("local", "0.0.0.0:80", "local address")
+var remoteAddr *string = flag.String("remote", "0.0.0.0:80", "remote address")
 
 func main() {
-	fmt.Println("my name is tcp proxy")
+	flag.Parse()
+	fmt.Printf("Listening: %v\nProxying: %v\n\n", *localAddr, *remoteAddr)
+
+	listener, err := net.Listen("tcp", *localAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		log.Println("[New connection] remote addr:", conn.RemoteAddr())
+		if err != nil {
+			log.Println("error accepting connection", err)
+			continue
+		}
+		go DoProxy(conn)
+	}
+}
+
+func DoProxy(conn net.Conn) {
+	conn2, err := net.Dial("tcp", *remoteAddr)
+	if err != nil {
+		log.Println("error dialing remote addr", err)
+		return
+	}
+	defer conn2.Close()
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		// send data
+		io.Copy(conn2, conn)
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		// receive data
+		io.Copy(conn, conn2)
+		wg.Done()
+	}()
+	wg.Wait()
+	log.Println("[Connection complete] remote addr:", conn.RemoteAddr())
 }
